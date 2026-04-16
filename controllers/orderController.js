4ffacf183ddda5f5c47cad2f82932e8a0eef783f
Ederson <jvedrson@gmail.com>
2026-04-16 02:37:56 -0400

    feat: update test scripts and add user routes tests

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,129 +2,229 @@ const mongodb = require("../db/database");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
-    // #swagger.tags = ["Orders"]
-    try {
-        const orders = await mongodb.getDatabase()
-            .collection("orders")
-            .find()
-            .toArray();
-        res.setHeader("Content-Type", "application/json");
-        res.status(200).json(orders);
-    } catch (err) {
-        res.status(500).json({ message: err.message || "Error retrieving orders." });
-    }
+  // #swagger.tags = ["Orders"]
+  try {
+    const orders = await mongodb
+      .getDatabase()
+      .collection("orders")
+      .find()
+      .toArray();
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(orders);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: err.message || "Error retrieving orders." });
+  }
 };
 
 const getSingle = async (req, res) => {
-    // #swagger.tags = ["Orders"]
-    try {
-        if (!ObjectId.isValid(req.params.id)) {
-            return res.status(400).json({ message: "Invalid order ID." });
-        }
-        const orderId = new ObjectId(req.params.id);
-        const order = await mongodb.getDatabase()
-            .collection("orders")
-            .findOne({ _id: orderId });
-        res.setHeader("Content-Type", "application/json");
-        if (order) {
-            res.status(200).json(order);
-        } else {
-            res.status(404).json({ message: "Order not found." });
-        }
-    } catch (err) {
-        res.status(500), json({ message: err.message || "Error retrieving the order." });
-        
+  // #swagger.tags = ["Orders"]
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: "Invalid order ID." });
     }
+    const orderId = new ObjectId(req.params.id);
+    const order = await mongodb
+      .getDatabase()
+      .collection("orders")
+      .findOne({ _id: orderId });
+    res.setHeader("Content-Type", "application/json");
+    if (order) {
+      res.status(200).json(order);
+    } else {
+      res.status(404).json({ message: "Order not found." });
+    }
+  } catch (err) {
+    (res.status(500),
+      json({ message: err.message || "Error retrieving the order." }));
+  }
 };
 
 const createOrder = async (req, res) => {
-    // // #swagger.tags = ["Orders"]
-    try {
-        const order = {
-            user_id: new ObjectId(req.params.user_id),
-            codigo: req.params.codigo,
-            products: req.body.products.map((p) => ({
-                product_id: new ObjectId(p.product_id),
-                name: p.name,
-                price_at_purchase: p.price_at_purchase,
-                quantity: p.quantity,
-            })),
-            total: req.body.total,
-            status: req.body.status,
-            created_at: new Date().toISOString(),
-        };
-        const response = await mongodb
-            .getDatabase()
-            .collection("orders")
-            .insertOne(order);
-        if (response.acknowledged) {
-            res.status(201).json({
-                message: "Order created successfully.",
-                id: response.insertedId,
-            });
-        } else {
-            res.status(500).json({ message: response.error || "Error creating the order." });
-        }
-    } catch (err) {
-        res.status(500).json({ message: response.error || "Error creating the order." });
+  // // #swagger.tags = ["Orders"]
+  try {
+    if (!ObjectId.isValid(req.body.user_id)) {
+      return res.status(400).json({ message: "Invalid user ID." });
     }
+
+    const user = await mongodb
+      .getDatabase()
+      .collection("users")
+      .findOne({ _id: new ObjectId(req.body.user_id) });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const productIds = req.body.products.map((p) => p.product_id);
+    const validProductIds = productIds.every((id) => ObjectId.isValid(id));
+    if (!validProductIds) {
+      return res.status(400).json({
+        message: "One or more product IDs are invalid.",
+      });
+    }
+
+    const productObjectIds = productIds.map((id) => new ObjectId(id));
+    console.log("Product Object IDs:", productObjectIds);
+
+    const products = await mongodb
+      .getDatabase()
+      .collection("products")
+      .find({ _id: { $in: productObjectIds } })
+      .toArray();
+
+    console.log("Found products:", products);
+    console.log("Requested product IDs:", productIds);
+
+    if (products.length !== productIds.length) {
+      return res
+        .status(400)
+        .json({ message: "One or more product IDs are invalid." });
+    }
+
+    const order = {
+      user_id: new ObjectId(req.body.user_id),
+      codigo: req.body.codigo,
+      products: req.body.products.map((p) => ({
+        product_id: new ObjectId(p.product_id),
+        name: p.name,
+        price_at_purchase: p.price_at_purchase,
+        quantity: p.quantity,
+      })),
+      total: req.body.total,
+      status: req.body.status,
+      created_at: new Date().toISOString(),
+    };
+    const response = await mongodb
+      .getDatabase()
+      .collection("orders")
+      .insertOne(order);
+    if (response.acknowledged) {
+      res.status(201).json({
+        message: "Order created successfully.",
+        id: response.insertedId,
+      });
+    } else {
+      res
+        .status(500)
+        .json({ message: response.error || "Error creating the order." });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: err.message || "Error creating the order." });
+  }
 };
 
 const updateOrder = async (req, res) => {
-    // #swagger.tags = ["Orders"]
+  // #swagger.tags = ["Orders"]
+  try {
     if (!ObjectId.isValid(req.params.id)) {
-        return res.status(400).json({ message: "Ivalid order ID." });
+      return res.status(400).json({ message: "Invalid order ID." });
     }
-    try {
-        const orderId = new ObjectId(req.params.id);
-        const order = {
-            user_id: new ObjectId(req.params.user_id),
-            codigo: req.params.codigo,
-            products: req.body.products.map((p) => ({
-                product_id: new ObjectId(p.product_id),
-                name: p.name,
-                price_at_purchase: p.price_at_purchase,
-                quantity: p.quantity,
-            })),
-            total: req.body.total,
-            status: req.body.status,
-            created_at: new Date().toISOString(),
-        };
-        const response = await mongodb
-            .getDatabase()
-            .collection("orders")
-            .replaceOne({ _id: orderId }, order);
-        if (response.modifiedCount > 0) {
-            res.status(200).json({ message: "Order updated succesfully." });
-        } else {
-            res.status(500).json({ message: response.error || "Error updating order. " });
-        }
-    } catch (err) {
-        res.status(500).json({ message: response.err || "Error updating order. " });
+
+    const findOrder = await mongodb
+      .getDatabase()
+      .collection("orders")
+      .findOne({ _id: new ObjectId(req.params.id) });
+
+    if (!findOrder) {
+      return res.status(404).json({ message: "Order not found." });
     }
+
+    if (!ObjectId.isValid(req.body.user_id)) {
+      return res.status(400).json({ message: "Invalid user ID." });
+    }
+
+    const user = await mongodb
+      .getDatabase()
+      .collection("users")
+      .findOne({ _id: new ObjectId(req.body.user_id) });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const productIds = req.body.products.map((p) => p.product_id);
+    const validProductIds = productIds.every((id) => ObjectId.isValid(id));
+    if (!validProductIds) {
+      return res.status(400).json({
+        message: "One or more product IDs are invalid.",
+      });
+    }
+
+    const productObjectIds = productIds.map((id) => new ObjectId(id));
+
+    const products = await mongodb
+      .getDatabase()
+      .collection("products")
+      .find({ _id: { $in: productObjectIds } })
+      .toArray();
+
+    if (products.length !== productIds.length) {
+      return res
+        .status(400)
+        .json({ message: "One or more product IDs are invalid." });
+    }
+
+    const orderId = new ObjectId(req.params.id);
+    const updateOrder = {
+      user_id: new ObjectId(req.body.user_id),
+      codigo: req.body.codigo,
+      products: req.body.products.map((p) => ({
+        product_id: new ObjectId(p.product_id),
+        name: p.name,
+        price_at_purchase: p.price_at_purchase,
+        quantity: p.quantity,
+      })),
+      total: req.body.total,
+      status: req.body.status,
+      created_at: new Date().toISOString(),
+    };
+    const response = await mongodb
+      .getDatabase()
+      .collection("orders")
+      .replaceOne({ _id: orderId }, updateOrder);
+    if (response.modifiedCount > 0) {
+      res.status(200).json({ message: "Order updated succesfully." });
+    } else {
+      res.status(500).json({ message: "Error updating order." });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Error updating order." });
+  }
 };
 
 const deleteOrder = async (req, res) => {
-    // #swagger.tags = ["Orders"]
+  // #swagger.tags = ["Orders"]
+  try {
     if (!ObjectId.isValid(req.params.id)) {
-        return res.status(400).json({ message: "Invalid order ID." });
+      return res.status(400).json({ message: "Invalid order ID." });
     }
-    try {
-        const orderId = new ObjectId(req.params.id);
-        const response = await mongodb
-            .getDatabase()
-            .collection("orders")
-            .deleteOne({ _id: orderId })
-        if (response.deletedCount > 0) {
-            res.status(200).json({ message: "Order deleted succesfully" });
-        } else {
-            res.status(500).json({ message: response.error || "Error deleting the order." })
-        }
-    } catch (err) {
-        res.status(500).json({ message: err.message || "Error deleting the order." })
+    const orderId = new ObjectId(req.params.id);
+    const response = await mongodb
+      .getDatabase()
+      .collection("orders")
+      .deleteOne({ _id: orderId });
+    if (response.deletedCount > 0) {
+      res.status(200).json({ message: "Order deleted succesfully" });
+    } else {
+      res
+        .status(500)
+        .json({ message: response.error || "Error deleting the order." });
     }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: err.message || "Error deleting the order." });
+  }
 };
 
 module.exports = {
-    getAll, getSingle, createOrder, updateOrder, deleteOrder
+  getAll,
+  getSingle,
+  createOrder,
+  updateOrder,
+  deleteOrder,
 };
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger-output.json");
-const dotenv = require('dotenv').config();
-const passport = require('passport');
-const session = require('express-session');
-const GitHubStrategy = require('passport-github2').Strategy;
+const dotenv = require("dotenv").config();
+const passport = require("passport");
+const session = require("express-session");
+const GitHubStrategy = require("passport-github2").Strategy;
 
 const cors = require("cors");
 
@@ -14,32 +14,39 @@ const port = process.env.PORT || 8080;
 const mongoDB = require("./db/database");
 
 app.use(express.json());
-app.use(session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-}))
-    .use(passport.initialize())
-    .use(passport.session())
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader(
-            'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-        );
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        next();
-    })
-    .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
-    .use(cors({ origin: '*' }))
-    .use("/", require("./routes"));
+app
+  .use(
+    session({
+      secret: "secret",
+      resave: false,
+      saveUninitialized: true,
+    }),
+  )
+  .use(passport.initialize())
+  .use(passport.session())
+  .use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+      allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "Z-Key",
+      ],
+    }),
+  )
+  .use("/", require("./routes"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL
-},
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
     async (accesToekn, refreshToken, profile, done) => {
       try {
         const db = mongoDB.getDatabase().collection("users");
@@ -48,37 +55,50 @@ passport.use(new GitHubStrategy({
           const newUser = {
             github_id: profile.id,
             name: profile.displayName || profile.username,
-            email: profile.emails && profile.emails[0] ? profile.emails[0].value : null,
+            email:
+              profile.emails && profile.emails[0]
+                ? profile.emails[0].value
+                : null,
             role: "admin",
-          }
+          };
           await db.insertOne(newUser);
-          user = newUser
+          user = newUser;
         }
         profile = { ...user };
 
         return done(null, profile);
-      } catch (err)
-      {
+      } catch (err) {
         return done(err, null);
-      }}
-));
+      }
+    },
+  ),
+);
 
 passport.serializeUser((user, done) => {
-    done(null, user)
+  done(null, user);
 });
 passport.deserializeUser((user, done) => {
-    done(null, user)
+  done(null, user);
 });
 
-app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out") });
+app.get("/", (req, res) => {
+  res.send(
+    req.session.user !== undefined
+      ? `Logged in as ${req.session.user.displayName}`
+      : "Logged Out",
+  );
+});
 
-app.get('/github/callback', passport.authenticate('github', {
-    failureRedirect: '/api-docs', session: false
-}),
-    (req, res) => {
-        req.session.user = req.user;
-        res.redirect('/');
-    }
+app.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/api-docs",
+    session: false,
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect("/");
+  },
 );
 
 mongoDB.initDatabase((err) => {
